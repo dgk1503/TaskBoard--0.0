cac9b7e26daa03d4b7c5e6ad7bf172a43b739e4b
dgk <dkarthikeya2008@gmail.com>
2025-10-02 21:37:31 +0530

    .

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
-import axios from 'axios'
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api"
-const api = axios.create({
-    baseURL:BASE_URL
-})
+import axios from "axios";
 
-export default api
+// in production, there's no localhost so we have to make this dynamic
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
+
+const api = axios.create({
+  baseURL: BASE_URL,
+});
+
+export default api;
