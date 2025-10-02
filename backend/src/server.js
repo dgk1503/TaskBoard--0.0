54ba8ddb4d37b099b29e8912cc97b1d46a45b457
dgk <dkarthikeya2008@gmail.com>
2025-10-02 11:26:01 +0530

    prepared for commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,9 +11,13 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5001
 const __dirname = path.resolve()
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors())
+
+}
 
 //middleware to parse JSON : req.body etc
-app.use(cors())
+
 app.use(express.json());
 app.use(rateLimiter);
 
@@ -27,7 +31,13 @@ app.use(rateLimiter);
 
 
 app.use('/api/notes',router)
-app.use(express.static(path.join(__dirname,"../frontend/dist")))
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+})
+}
 connectDB().then(() => {
 
     app.listen(PORT,() => {
