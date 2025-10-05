04572231d6eab14a67310e7c7bd1fe100b27895c
dgk <dkarthikeya2008@gmail.com>
2025-10-05 16:43:22 +0530

    Backdrop filter

--- frontend/src/components/NoteNotFound.jsx
+++ frontend/src/components/NoteNotFound.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router";
 const NoteNotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center py-16 space-y-6 max-w-md mx-auto text-center">
-      <div className="bg-primary/10 rounded-sm p-8">
+      <div className="bg-primary/10 rounded-sm p-8 backdrop-blur-2xl">
         <SearchX className="size-10 text-primary backdrop-blur-3xl"></SearchX>
       </div>
       <h3 className="text-2xl font-bold">No Notes Found.</h3>
